9a9106e662970b038d5849aa9142e6919a0591d3
ege.sahin <ege.sahin@ulakhaberlesme.com.tr>
2022-12-13 12:30:04 +0300

    4th commit

--- hello.java
+++ hello.java
@@ -2,5 +2,6 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
         System.out.println("Hello, Jenkins!"); 
+        System.out.println("1, 2, 3, 4, 5"); 
     }
 }
